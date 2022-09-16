b4c3079fcfd9a4bec9d539f0fd2f9cf229a5722b
edwardalex <olooedwards69@gmail.com>
2022-09-16 02:55:27 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Installing flask 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider =>  'gem',
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
