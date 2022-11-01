2baf871f7c72d40b414822fcab77322cd1bfa30f
edwardalex <olooedwards69@gmail.com>
2022-11-01 02:13:52 -0700

    updated

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
-"""
-    Uses the reddit API for get the all hot posts
-"""
+"""Recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit"""
+
 import requests
-from sys import argv
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """Get the all host posts"""
-    if after is None:
-        return []
+def recurse(subreddit, hot_list=[], after=None):
+    """Returns list of hot articles for a given subreddit"""
 
-    url_sred_inf = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    url_sred_inf += "?limit=100&after={}".format(after)
-    headers = {'user-agent': 'request'}
-    response = requests.get(url_sred_inf, headers=headers,
-                            allow_redirects=False)
-    if str(response) != "<Response [200]>":
+    header = {"User-Agent": "Jose98"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=header, allow_redirects=False)
+
+    # Establish after fullname
+    nxt = None
+    if after:
+        nxt = {'after': after}
+    response = requests.get(url,
+                            headers=header, allow_redirects=False, params=nxt)
+
+    if response.status_code != 200:
         return None
-    r_json = response.json()
-    hot_posts_json = r_json.get("data").get("children")
-    for post in hot_posts_json:
-        hot_list.append(post.get("data").get("title"))
-    return hot_list + recurse(subreddit, [], r_json.get("data").get("after"))
+
+    after = response.json().get('data').get('after')
+    # append articles for the given subreddit in the current page
+    for post in response.json().get('data').get('children'):
+        hot_list.append(post.get('data').get('title'))
+
+    # If there are more next pages call recurse function again
+    if after:
+        return recurse(subreddit, hot_list, after)
+    # otherwise return the list with all hot articles for the given subreddit
+    return hot_list
